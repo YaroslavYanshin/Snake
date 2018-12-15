fc0c7f84af8222ab589fe47a9ab801f6591d7c90
Władysław Jakołcewicz <vladislavby96@gmail.com>
2018-12-15 22:45:54 +0100

    Create class.

---
+++ Snake/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Game
+    {
+        private int points;
+        public Game()
+        {
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+            Console.CursorVisible = false;
+        }
+    }
+}
